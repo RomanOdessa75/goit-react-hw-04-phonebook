8963e75136f703b09b30bc86dffa58e2ac08d02c
RomanOdessa75 <romeowrc@ukr.net>
2023-05-25 19:11:53 +0300

    react-hw-04-phonebook-code-optimization-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,33 +5,22 @@ import { Filter } from './Filter/Filter';
 import { ContactsList } from './ContactsList/ContactsList';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => contactsFromLocalStorage());
 
   const [filter, setFilter] = useState('');
 
-  const [isAdded, setIsAdded] = useState(true);
+  function contactsFromLocalStorage() {
+    const savedContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (savedContacts) {
+      return savedContacts;
+    } else {
+      return [];
+    }
+  }
 
   useEffect(() => {
-    if (isAdded) {
-      const contactsFromLocalStorage = localStorage.getItem('contactList');
-
-      if (contactsFromLocalStorage !== 'undefined') {
-        const parsedContacts = JSON.parse(contactsFromLocalStorage);
-
-        if (parsedContacts) {
-          setContacts(parsedContacts);
-        }
-      }
-      setIsAdded(false);
-    } else {
-      localStorage.setItem('contactList', JSON.stringify(contacts));
-    }
-  }, [contacts, isAdded]);
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const handleChange = e => {
     const { value } = e.target;
